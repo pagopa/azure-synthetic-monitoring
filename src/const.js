268a11b17039fd6c954cff37d386200e3c721cf3
Marco Mari <130982006+mamari90@users.noreply.github.com>
2024-02-08 17:37:55 +0100

    feat: Added certSuccess field (#9)

--- src/const.js
+++ src/const.js
@@ -1,5 +1,5 @@
 const keysForTelemetry = ['success', 'message', 'duration', 'runLocation'];
-const keysForEvent = ['duration', 'targetStatus', 'targetExpirationTimestamp', 'httpStatus', 'targetTlsVersion', 'targetExpireInDays'] ;
+const keysForEvent = ['duration', 'targetStatus', 'targetExpirationTimestamp', 'httpStatus', 'targetTlsVersion', 'targetExpireInDays', 'certSuccess'] ;
 const keysForEventProperties = ['domain', 'checkCert'];
 const TLS_VERSION_KEY = "TLS_VERSION"
 const START_TIMESTAMP_KEY = "x-request-timestamp"
