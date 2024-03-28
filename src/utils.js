b7dba4bf688dcdc4d43437ed01a6fe9935ecb7c5
mamari90 <130982006+mamari90@users.noreply.github.com>
2024-03-28 16:36:43 +0100

    connection timeout ssl test

--- src/utils.js
+++ src/utils.js
@@ -94,7 +94,7 @@ function certChecker(sslClient){
         }
 
         if (metricContext.monitoringConfiguration.checkCertificate){
-            return sslClient.get(url.host, 2000)
+            return sslClient.get(url.host, 20)
                 .then(statics.certResponseElaborator(metricContext))
                 .catch(statics.certErrorElaborator(metricContext))
         } else {
