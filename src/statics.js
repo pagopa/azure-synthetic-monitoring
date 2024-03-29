02e63b641381459b4d8e792e9553b5c0ac11e6c4
mamari90 <130982006+mamari90@users.noreply.github.com>
2024-03-29 16:56:10 +0100

    restored connection timeout

--- src/statics.js
+++ src/statics.js
@@ -195,7 +195,7 @@ function buildRequest(monitoringConfiguration){
                 return true; //every status code should be treated as a valid code (it will be checked later)
             },
             timeout: monitoringConfiguration.httpClientTimeout,
-            signal: AbortSignal.timeout(30000)
+            signal: AbortSignal.timeout(monitoringConfiguration.httpConnectionTimeout)
     }
 
     if (monitoringConfiguration.headers) {
